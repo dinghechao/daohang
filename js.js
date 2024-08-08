4cfc9382a252afa2f3ddb973cf7d27e121083d3c
丁合超 <625030319@qq.com>
2024-08-08 15:11:20 +0800

    Update js.js

--- js.js
+++ js.js
@@ -186,23 +186,23 @@ $(function(){
   var search = {
     data: [{
       name: '百度',
-      img: '/images/logo.jpg',
+      img: 'logo.jpg',
       url: 'https://www.baidu.com/s?wd='
     }, {
       name: '谷歌',
-      img: '/images/logo_2.jpg',
+      img: 'logo_2.jpg',
       url: 'https://www.google.com/search?q='
     }, {
       name: '必应',
-      img: '/images/logo_3.jpg',
+      img: 'logo_3.jpg',
       url: 'https://cn.bing.com/search?q='
     }, {
       name: '好搜',
-      img: '/images/logo_4.jpg',
+      img: 'logo_4.jpg',
       url: 'https://www.so.com/s?q='
     }, {
       name: '搜狗',
-      img: '/images/logo_5.jpg',
+      img: 'logo_5.jpg',
       url: 'https://www.sogou.com/web?query='
     }]
   }
